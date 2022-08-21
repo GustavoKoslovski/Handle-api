8474fbd566cfe1f52ace093582ac1eea63501b32
layalWanni <nissolelo03@gmail.com>
2022-08-21 19:47:37 -0400

    repository test

--- src/test/java/br/com/uniamerica/handleapi/repository/ProdutoRepositoryTest.java
+++ src/test/java/br/com/uniamerica/handleapi/repository/ProdutoRepositoryTest.java
@@ -1,4 +1,15 @@
 package br.com.uniamerica.handleapi.repository;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class ProdutoRepositoryTest {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
 }
